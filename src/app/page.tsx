d60ffdb842732b5ef84a624977e399c07a60b21d
Alexandre <em_alexandre@yahoo.co.jp>
2024-10-03 21:04:16 +0200

    Add Meetup group event widget
    Fix description position on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { cn } from '@/lib/utils';
 
 const JapaneseTeacherLanding = () => {
   return (
-    <div className="relative">
+    <div className="relative overflow-x-hidden">
       <main className="container mx-auto px-4 py-8 m-5 max-w-[1100px]">
         <section id="hero" className="text-center py-16">
           <TypographyH1>Keita Yasue</TypographyH1>
@@ -19,40 +19,55 @@ const JapaneseTeacherLanding = () => {
           <div className="mt-8 inline-block animate-bounce">
             <Underline color="#ff920f" width={200} height={50} />
           </div>
-          <div className="hidden sm:flex flex-wrap justify-center">
+          <div className="flex flex-wrap justify-center items-center">
             <Professor width={500} height={200} />
             <div className="absolute bg-[#b07edf45] z-[-90] w-[200px] h-[200px] rounded-full"></div>
           </div>
         </section>
 
-        <section id="about" className="mb-16">
+        <div className="max-w-[600px] block lg:hidden mb-5 text-center sm:text-start">
           <TypographyH2>Welcome to My Japanese Learning World!</TypographyH2>
           <TypographyMuted>
             こんにちは (Konnichiwa)! I&apos;m Keita Yasue, a passionate Japanese language teacher. Whether you&apos;re a beginner
             or looking to polish your skills, I&apos;m here to guide you on your Japanese language journey.
           </TypographyMuted>
-        </section>
+        </div>
 
-        <section id="services" className="mb-16">
+        <section id="about" className="mb-16">
           <TypographyH2>My Services</TypographyH2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <ServiceCard
-              icon={<Book />}
-              title="One-on-one Lessons"
-              description="Personalized online or in-person Japanese lessons"
-            />
-            {/* <ServiceCard icon={<Users />} title="Group Classes" description="Interactive group classes for all levels" /> */}
-            <ServiceCard icon={<Briefcase />} title="Business Japanese" description="Specialized lessons for professionals" />
-            <ServiceCard
-              icon={<Award />}
-              title="JLPT Preparation"
-              description="Focused training for the Japanese Language Proficiency Test"
-            />
-            <ServiceCard
-              icon={<Paperclip />}
-              title="Cultural Workshops"
-              description="Explore calligraphy, origami, and tea ceremony"
-            />
+          <div>
+            <div className="relative flex flex-wrap justify-between items-center">
+              <img src="/keita.png" alt="keita yasue" width={400} />
+              <div className="bg-[#ff920f70] w-[400px] h-[400px] absolute top-0 z-[-1000] rounded-full" />
+              <div className="max-w-[600px] hidden lg:block">
+                <TypographyH2>Welcome to My Japanese Learning World!</TypographyH2>
+                <TypographyLead>
+                  こんにちは (Konnichiwa)! I&apos;m Keita Yasue, a passionate Japanese language teacher. Whether you&apos;re a
+                  beginner or looking to polish your skills, I&apos;m here to guide you on your Japanese language journey.
+                </TypographyLead>
+              </div>
+            </div>
+            <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <ServiceCard
+                  icon={<Book />}
+                  title="One-on-one Lessons"
+                  description="Personalized online or in-person Japanese lessons"
+                />
+                {/* <ServiceCard icon={<Users />} title="Group Classes" description="Interactive group classes for all levels" /> */}
+                <ServiceCard icon={<Briefcase />} title="Business Japanese" description="Specialized lessons for professionals" />
+                <ServiceCard
+                  icon={<Award />}
+                  title="JLPT Preparation"
+                  description="Focused training for the Japanese Language Proficiency Test"
+                />
+                <ServiceCard
+                  icon={<Paperclip />}
+                  title="Cultural Workshops"
+                  description="Explore calligraphy, origami, and tea ceremony"
+                />
+              </div>
+            </div>
           </div>
         </section>
 
@@ -78,6 +93,11 @@ const JapaneseTeacherLanding = () => {
           </div>
           <ContactForm />
         </section>
+
+        <section id="meetup" className="mt-16">
+          <TypographyH2>Meetup events</TypographyH2>
+          <iframe src="https://widgets.sociablekit.com/meetup-group-events/iframe/25471111" width="100%" height="690"></iframe>
+        </section>
       </main>
 
       <footer className="py-4 mt-16">
@@ -98,10 +118,12 @@ type ServiceCardProps = {
 };
 
 const ServiceCard = ({ icon, title, description }: ServiceCardProps) => (
-  <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300">
-    <div className="text-[#fffcf7] bg-primary-color w-fit p-3 rounded-full">{icon}</div>
-    <TypographyH3>{title}</TypographyH3>
-    <TypographyMuted>{description}</TypographyMuted>
+  <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 flex items-center">
+    <div className="text-[#fffcf7] bg-primary-color w-fit h-fit p-3 rounded-full mr-5">{icon}</div>
+    <div>
+      <TypographyH3>{title}</TypographyH3>
+      <TypographyMuted>{description}</TypographyMuted>
+    </div>
   </div>
 );
 
