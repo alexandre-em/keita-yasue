f5d96797fe62b328d1dcfb44a5ac23c13bd0b47b
Alexandre Em <a.em@qivia.fr>
2025-04-28 23:52:29 +0200

    fix: from email sender

--- actions/mail.ts
+++ actions/mail.ts
@@ -1,8 +1,14 @@
-'use server';
-import { Attachment, EmailParams, MailerSend, Recipient, Sender } from 'mailersend';
+"use server";
+import {
+  Attachment,
+  EmailParams,
+  MailerSend,
+  Recipient,
+  Sender,
+} from "mailersend";
 
-import { admin } from '@/constants/admin';
-import { randomUUID } from 'crypto';
+import { admin } from "@/constants/admin";
+import { randomUUID } from "crypto";
 
 type ContactType = {
   email: string;
@@ -21,16 +27,23 @@ type IcsType = {
 function formatToICSDate(date: string | Date | number) {
   const newDate = new Date(date);
   const year = newDate.getUTCFullYear();
-  const month = String(newDate.getUTCMonth()).padStart(2, '0');
-  const day = String(newDate.getUTCDate()).padStart(2, '0');
-  const hours = String(newDate.getUTCHours()).padStart(2, '0');
-  const minutes = String(newDate.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(newDate.getUTCSeconds()).padStart(2, '0');
+  const month = String(newDate.getUTCMonth()).padStart(2, "0");
+  const day = String(newDate.getUTCDate()).padStart(2, "0");
+  const hours = String(newDate.getUTCHours()).padStart(2, "0");
+  const minutes = String(newDate.getUTCMinutes()).padStart(2, "0");
+  const seconds = String(newDate.getUTCSeconds()).padStart(2, "0");
   const result = `${year}${month}${day}T${hours}${minutes}${seconds}Z`;
   return result;
 }
 
-function generateICS({ uid, summary, description, location, startTime, endTime }: IcsType) {
+function generateICS({
+  uid,
+  summary,
+  description,
+  location,
+  startTime,
+  endTime,
+}: IcsType) {
   return `BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//YourApp//ZoomIntegration//EN
@@ -52,11 +65,15 @@ END:VEVENT
 END:VCALENDAR`;
 }
 
-export const sendMessageMail = async (to: ContactType, from: ContactType, message: string) => {
+export const sendMessageMail = async (
+  to: ContactType,
+  from: ContactType,
+  message: string,
+) => {
   const mailersend = new MailerSend({
-    apiKey: process.env['NEXT_PUBLIC_MAILSENDER_API_TOKEN'] ?? '',
+    apiKey: process.env["NEXT_PUBLIC_MAILSENDER_API_TOKEN"] ?? "",
   });
-  const author = new Sender('contact@keita-yasue.uk', 'Keita Yasue | Contact');
+  const author = new Sender("contact@keita-yasue.uk", "Keita Yasue | Contact");
   const recipients = [new Recipient(to.email, to.name)];
 
   const personalization = [
@@ -70,7 +87,7 @@ export const sendMessageMail = async (to: ContactType, from: ContactType, messag
         comment: {
           date: new Date().toLocaleDateString(),
         },
-        account_name: 'Keita Yasue',
+        account_name: "Keita Yasue",
         message,
       },
     },
@@ -81,7 +98,9 @@ export const sendMessageMail = async (to: ContactType, from: ContactType, messag
     .setTo(recipients)
     .setReplyTo(new Recipient(from.email, from.name))
     .setSubject(`New message from ${to.name}`)
-    .setTemplateId(process.env['NEXT_PUBLIC_MAILSENDER_MESSAGE_TEMPLATE_ID'] ?? '')
+    .setTemplateId(
+      process.env["NEXT_PUBLIC_MAILSENDER_MESSAGE_TEMPLATE_ID"] ?? "",
+    )
     .setPersonalization(personalization);
 
   await mailersend.email.send(emailParams);
@@ -93,11 +112,14 @@ type ReservationData = {
   jpTime: string;
 };
 
-export const sendReservationMail = async (to: ContactType, data: ReservationData) => {
+export const sendReservationMail = async (
+  to: ContactType,
+  data: ReservationData,
+) => {
   const mailersend = new MailerSend({
-    apiKey: process.env['NEXT_PUBLIC_MAILSENDER_API_TOKEN'] ?? '',
+    apiKey: process.env["NEXT_PUBLIC_MAILSENDER_API_TOKEN"] ?? "",
   });
-  const author = new Sender('contact@keita-yasue.uk', 'Keita Yasue | Contact');
+  const author = new Sender("contact@keita-yasue.uk", "Keita Yasue | Contact");
   const recipients = [new Recipient(to.email, to.name)];
 
   const personalization = [
@@ -108,7 +130,7 @@ export const sendReservationMail = async (to: ContactType, data: ReservationData
         date: data.date,
         time: data.time,
         jptime: data.jpTime,
-        account_name: 'Keita Yasue',
+        account_name: "Keita Yasue",
       },
     },
   ];
@@ -116,10 +138,12 @@ export const sendReservationMail = async (to: ContactType, data: ReservationData
   const emailParams = new EmailParams()
     .setFrom(author)
     .setTo(recipients)
-    .setCc([new Recipient(admin.email, admin.name)])
-    .setReplyTo(new Recipient(admin.email, admin.name))
-    .setSubject(`New reservation with Keita the ${data.date}`)
-    .setTemplateId(process.env['NEXT_PUBLIC_MAILSENDER_RESERVATION_TEMPLATE_ID'] ?? '')
+    .setCc([new Recipient(admin.email, "Keita Yasue")])
+    .setReplyTo(new Recipient(admin.email, "Keita Yasue"))
+    .setSubject(`Your lesson with Keita the ${data.date}`)
+    .setTemplateId(
+      process.env["NEXT_PUBLIC_MAILSENDER_RESERVATION_TEMPLATE_ID"] ?? "",
+    )
     .setPersonalization(personalization);
 
   await mailersend.email.send(emailParams);
@@ -131,18 +155,25 @@ type ValidationReservationData = {
   meetingLink: string;
 };
 
-export const sendValidationReservationMail = async (to: ContactType, data: ValidationReservationData) => {
+export const sendValidationReservationMail = async (
+  to: ContactType,
+  data: ValidationReservationData,
+) => {
   const mailersend = new MailerSend({
-    apiKey: process.env['NEXT_PUBLIC_MAILSENDER_API_TOKEN'] ?? '',
+    apiKey: process.env["NEXT_PUBLIC_MAILSENDER_API_TOKEN"] ?? "",
   });
-  const author = new Sender('contact@keita-yasue.uk', 'Keita Yasue | Contact');
+  const author = new Sender("contact@keita-yasue.uk", "Keita Yasue | Contact");
   const recipients = [new Recipient(to.email, to.name)];
   const { startDate, endDate } = data;
 
-  const dateLocale = startDate.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+  const dateLocale = startDate.toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const timeLocale = `${startDate.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', timeZone })} to ${endDate.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', timeZone })}`;
-  const jpTime = `${startDate.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', timeZone: 'Asia/Tokyo' })} to ${endDate.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', timeZone: 'Asia/Tokyo' })}`;
+  const timeLocale = `${startDate.toLocaleString("en-US", { hour: "2-digit", minute: "numeric", timeZone })} to ${endDate.toLocaleString("en-US", { hour: "2-digit", minute: "numeric", timeZone })}`;
+  const jpTime = `${startDate.toLocaleString("en-US", { hour: "2-digit", minute: "numeric", timeZone: "Asia/Tokyo" })} to ${endDate.toLocaleString("en-US", { hour: "2-digit", minute: "numeric", timeZone: "Asia/Tokyo" })}`;
 
   const personalization = [
     {
@@ -153,30 +184,39 @@ export const sendValidationReservationMail = async (to: ContactType, data: Valid
         time: timeLocale,
         jptime: jpTime,
         meetinglink: data.meetingLink,
-        account_name: 'Keita Yasue',
+        account_name: "Keita Yasue",
       },
     },
   ];
 
   const content = generateICS({
     uid: randomUUID(),
-    summary: 'Lesson with Keita the ' + dateLocale,
-    description: 'Join Zoom Meeting: ' + data.meetingLink,
+    summary: "Lesson with Keita the " + dateLocale,
+    description: "Join Zoom Meeting: " + data.meetingLink,
     location: data.meetingLink,
     startTime: startDate,
     endTime: endDate,
   });
 
-  const attachmentContent = Buffer.from(content).toString('base64');
+  const attachmentContent = Buffer.from(content).toString("base64");
 
   const emailParams = new EmailParams()
     .setFrom(author)
     .setTo(recipients)
-    .setCc([new Recipient(admin.email, 'Keita Yasue')])
-    .setReplyTo(new Recipient(admin.email, 'Keita Yasue'))
+    .setCc([new Recipient(admin.email, "Keita Yasue")])
+    .setReplyTo(new Recipient(admin.email, "Keita Yasue"))
     .setSubject(`Your lesson booking for the ${dateLocale} has been validated`)
-    .setAttachments([new Attachment(attachmentContent, `invite-${to.name.trim()}-${formatToICSDate(startDate)}.ics`)])
-    .setTemplateId(process.env['NEXT_PUBLIC_MAILSENDER_VALIDATION_RESERVATION_TEMPLATE_ID'] ?? '')
+    .setAttachments([
+      new Attachment(
+        attachmentContent,
+        `invite-${to.name.trim()}-${formatToICSDate(startDate)}.ics`,
+      ),
+    ])
+    .setTemplateId(
+      process.env[
+        "NEXT_PUBLIC_MAILSENDER_VALIDATION_RESERVATION_TEMPLATE_ID"
+      ] ?? "",
+    )
     .setPersonalization(personalization);
 
   await mailersend.email.send(emailParams);
